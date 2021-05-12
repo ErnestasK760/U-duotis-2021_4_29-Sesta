d1f6d21403cb63ea71882c3f2d6e9186f7451339
ErnestasK760 <82801762+ErnestasK760@users.noreply.github.com>
2021-05-12 12:27:27 +0300

    Gallery update

--- logic.js
+++ logic.js
@@ -1,70 +1,76 @@
-// const container = document.querySelector('.grid-container')
-// const URL = 'https://source.unsplash.com/random/1280x721'
 
-
-// function loadImages(numImages = 4) {
-//     let i = 0;
-//     while(i < numImages){
-//         const img = document.createElement('img')
-//         img.src = `${URL}`;
-//         container.appendChild(img);
-//         i++;
-//     }
-// }
-
-// loadImages()
-
-// var img = document.createElement("img");
-// img.src = "http://www.google.com/intl/en_com/images/logo_plain.png";
-// var src = document.getElementById("header");
-// src.appendChild(img);
-
-var currentscrollHeight = 0;
-var count = 0;
-let sig = 20;
-ScrollDebounce = true;
- 
-$(window).on("scroll", function () {
- 
-  const scrollHeight = $(document).height();
-  const scrollPos = Math.floor($(window).height() + $(window).scrollTop());
-  const isBottom = scrollHeight - 100 < scrollPos;
-  // const list = $(".row");
-  // console.log(list);
-  const list2 = $(".container-fluid ");
-  // console.log(list2);
-  
- 
-  if (isBottom && currentscrollHeight < scrollHeight) { 
- 
-      
-   
-    if (ScrollDebounce) { 
-        ScrollDebounce = false;
-      
-    for(var i = 0; i < 2; i++) {
-   
-      sig++;
-      // console.log(sig);
-      list2.append(`  <div class="row ">
-      <div class="col-sm-6">
-        <div class="thumbnail">
-          <img class="float-left" src="https://source.unsplash.com/random/1800x1400?sig=`+sig+`'" alt="unsplash" >
-        </div>
-      </div>
-      <div class="col-sm-6">
-        <div class="thumbnail">
-          <img class="img float-right" src="https://source.unsplash.com/random/1800x1400?sig=`+ ++sig +`"  alt="unsplash">
-        </div>
-    </div>
-  </div>`
-      );
-      count++;
-     
+//initial load
+$(document).ready(function() {
+fetch("https://api.unsplash.com/photos/random?client_id=ypwAoVfL6vYqtraKuUSYtWDO4LF732P9XlmNyI4bPAw&count=4")
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    for(var i = 0; i < data.length; i++) {
+        console.log(data[i].urls.regular);
+        $(".row").append(`
+        <div class="col-md-6 grid-item">
+            <div class="card mb-3">
+                <img src="${data[i].urls.regular}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${data[i].user.username}</h5>
+                <p class="card-text">"${data[i].alt_description}"</p>
+                <p class="card-text"><small class="text-muted">Likes : ${data[i].likes}</small></p>
+            </div>
+        </div>`)
     }
-    console.log(ScrollDebounce);
-    setTimeout(function () { ScrollDebounce = true; }, 700); 
-    currentscrollHeight = scrollHeight;
-  }
-}
-});
+    var iso = new Isotope('.grid',{
+        itemSelector: '.grid-item',
+        stagger: 30,
+        getSortData: {
+        title: '.card-title'
+    },
+    masonry: {
+        columnWidth: '.grid-sizer',
+        gutter: '.grid-gutter-sizer'
+    },
+
+    sortBy: 'title',
+    sortAscending: true
+
+    });
+    var sortByGroup = document.querySelector('.sort-by-button');
+    sortByGroup.addEventListener('click', function( event ){
+        
+        if(!matchesSelector(event.target,'.btn')){
+            return;
+        } 
+        var sortValue = event.target.getAttribute('data-sort-value');
+        var sortDirection = event.target.getAttribute('data-sort-direction');
+        var isAscending = (sortDirection == 'asc');
+        
+        iso.arrange({
+            sortBy: sortValue,
+            sortAscending: isAscending
+        });
+        
+        console.log(sortValue,sortDirection,isAscending)
+        });
+      }    
+)});
+
+
+
+
+
+
+// var iso = new Isotope('.grid',{
+//     itemSelector: '.grid-item',
+//     stagger: 30,
+//     getSortData: {
+//         title: '.card-title'
+//     },
+//     masonry: {
+//         columnWidth: '.grid-sizer',
+//         gutter: '.grid-gutter-sizer'
+//     },
+
+//     sortBy: 'title',
+//     sortAscending: true
+
+//         });
+//     }
